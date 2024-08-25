7b281b7d834e43247828a7cf51c7388b054f5036
JaewonYooTime <dkrkvudghk@naver.com>
2024-08-26 03:54:23 +0900

    test

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import config from './apikey.js'
+// import config from './apikey.js'
 
 // Get references to DOM elements
 const clockElement = document.getElementById('clock');
@@ -36,8 +36,8 @@ backgroundImageElement.src = randomImage;
 
 // Weather and location
 function getWeather(lat, lon) {
-  const apiKey = config.API_KEY; // Replace with your OpenWeather API key
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  const apiKey = config.API_KEY;
+  // const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 
   fetch(url)
     .then(response => response.json())
